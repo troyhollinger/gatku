82749cdff4fbb821a41c4fbcd3a0d8ad7601f54d
Austen Payan <austenpayan@gmail.com>
2015-05-04 12:21:47 -0700

    add database password environment variable

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'forge',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => $_ENV['db_password'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
