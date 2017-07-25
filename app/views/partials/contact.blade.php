17e1979960bdffa6addbceb2e39bd2ecdb84b3ba
Greg McIntyre <nocolong@gmail.com>
2017-07-24 18:43:47 -0700

    footer changes

--- app/views/partials/contact.blade.php
+++ app/views/partials/contact.blade.php
@@ -7,28 +7,39 @@
 	</div>
 	<div class="contact-bottom">
 		<div class="home-container">
-			<h2 class="bold">GATKU</h2>	
-			<p class="contact-bottom-margin"><span class="bold">USA</span> : 1250 Pioneer Way Suite H, El Cajon, CA 92020 <br> <span class="bold">AU</span> : Beach Street Fremantle, Wa 6160</p>
+			<div class="contact-bottom-margin footer-addresses">
+				<div>
+					<img class="country-icon" src="{{ asset('/img/Site_GATKU_Footer-USA.png') }}">
+					1250 Pioneer Way Suite H, El Cajon, CA 92020 USA
+				</div>
+				<div>
+					<img class="country-icon" src="{{ asset('/img/Site_GATKU_Footer-AU.png') }}">
+					Beach Street Fremantle, Wa 6160 AU
+				</div>
+				<div>
+				<a href="mailto:dustin@gatku.com" class="contact-email-button"><div class="button info-bg">Email</div></a>
+				<a href="tel:6195073860" class="contact-phone-button"><div class="button info-bg">Phone</div></a>
 
-			<a href="mailto:dustin@gatku.com" class="contact-email-button"><div class="button info-bg">Email</div></a>
-			<a href="tel:6195073860" class="contact-phone-button"><div class="button info-bg">Phone</div></a>
-
-			<p class="inline left contact-email">Email : <a href="mailto:dustin@gatku.com"><span class="underline">dustin@gatku.com</span></a></p>
-			<p class="inline right contact-phone contact-bottom-margin">Phone : +001 619 507-3860</p>
-
-			<div class="contact-social-container clear contact-bottom-margin">
-
-				<a href="https://www.instagram.com/gatkupolespears/" target="_blank"><i class="fa fa-instagram"></i></a>
-				<a href="http://www.facebook.com/gatku" target="_blank"><i class="fa fa-facebook"></i></a>
-				<a href="http://www.twitter.com/gatku" target="_blank"><i class="fa fa-twitter"></i></a>
-				<a href="http://www.youtube.com/ryangattoni" target="_blank"><i class="fa fa-youtube-play"></i></a>
-
+				<p class="contact-email"><a href="mailto:dustin@gatku.com">dustin@gatku.com</a></p>
+				<p class="contact-phone contact-bottom-margin">619 507 3860</p>
+				</div>
+				<div class="tagline-container">
+					<img src="{{ asset('/img/Site_GATKU_Footer-TaglineBox.png') }}">
+				</div>
 			</div>
 
-			<p class="credits"><a href="{{ route('media') }}">Media</a>  |  &copy; {{ date('Y') }}  GATKU Polespears - All Rights Reserved.  |  <a href="http://troyhollinger.com">Made by Troy</a></p>
+			<div class="contact-social-container clear contact-bottom-margin">
+				<div class="social-media-icons">
+			
+				<a href="http://www.facebook.com/gatku" target="_blank"><i class="fa fa-facebook"></i></a>
+				<a href="http://www.twitter.com/gatku" target="_blank"><i class="fa fa-twitter"></i></a>
+				<a href="https://www.instagram.com/gatkupolespears/" target="_blank"><i class="fa fa-instagram"></i></a>
+				<a href="http://www.youtube.com/ryangattoni" target="_blank"><i class="fa fa-youtube-play"></i></a>
+				</div>
 
-		</div>
-		
+				<p class="credits"><a href="{{ route('media') }}">Media</a>  |  Copyright &copy; {{ date('Y') }}  GATKU Polespears - All rights reserved.  |  <a href="http://troyhollinger.com">Made By Troy</a></p>
+			</div>
+
+		</div>		
 	</div>
-
 </div>
