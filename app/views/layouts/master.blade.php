ab70e4a20c054df4d390e9fd47e4dff48ab782bf
Daniel Tyack <31866505+danieltyack@users.noreply.github.com>
2017-09-25 09:12:52 -0700

    Update master.blade.php
    Updated google analytics code

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -113,7 +113,7 @@
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-46130866-1', 'auto');
+          ga('create', 'UA-46881632-1', 'auto');
           ga('send', 'pageview');
 
         </script>
