4e2122a0f028f777f91a82b4747adafb9269635d
Marcin Wojcik <marcincyniu@gmail.com>
2018-02-22 17:13:01 -0800

    Feature/71 Revert AU but without link.

--- app/views/pages/product.blade.php
+++ app/views/pages/product.blade.php
@@ -168,7 +168,7 @@
 
 			<div class="clear"></div>
 
-			<p class="special-message"><span class="bold">Free Shipping</span> on orders over <span class="bold">$300</span> <span class="detail bold">USA ONLY</span></p>
+			<p class="special-message"><span class="bold">Free Shipping</span> on orders over <span class="bold">$300</span> <span class="detail bold">USA + AU ONLY</span></p>
 
 		@endif
 
